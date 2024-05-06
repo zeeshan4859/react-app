2b76644a8b76313b0e130729c521d66995331500
zeeshan4859 <zeeshan4859@gmial.com>
2024-05-06 15:05:23 +0500

    3rd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,24 @@
-import Calculator from "./components/Calculator";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <>
-      <Calculator />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is changed code.......
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
